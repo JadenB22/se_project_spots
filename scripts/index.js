f35046df8dc2057267d9c0d8ebdf9519f6cd38f9
Jaden Barron <ejaden1@gmail.com>
2025-03-01 09:35:51 -0600

    lots of changes to the modal and cards

--- scripts/index.js
+++ scripts/index.js
@@ -25,20 +25,43 @@ const initialCards = [
   },
 ];
 
-//profileEdit Section
+// Profile Edit Section
 const profileEditButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editModalCloseBtn = document.querySelector(".modal__close-btn");
+
+// Profile Name Input Section
 const profileName = document.querySelector(".profile__name");
+const editModalNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+
+// Profile Description Input Section
 const profileDescription = document.querySelector(".profile__description");
+const editModalDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+// Form Submission
+const editFormElement = editProfileModal.querySelector(".modal__form");
+
 function openModal() {
+  editModalNameInput.value = profileName.textContent; // Fill profile name form field
+  editModalDescriptionInput.value = profileDescription.textContent; // Fill description form field
   editProfileModal.classList.add("modal_opened");
+}
+
+function handleFormEditSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value; // Input to text
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
 }
 
 function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
+
 profileEditButton.addEventListener("click", openModal);
-{
-}
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleFormEditSubmit);
